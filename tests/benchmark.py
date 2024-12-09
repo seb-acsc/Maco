9ec172631d5e550834ac18a56ba71c680a651a09
seb-acsc <114536960+seb-acsc@users.noreply.github.com>
2024-12-09 02:31:16 +0000

    # This is a combination of 11 commits. # This is the 1st commit message:
    add benchmarking
    
    # This is the commit message #2:
    
    standalone package install
    
    # This is the commit message #3:
    
    much faster
    
    # This is the commit message #4:
    
    remove extra argument
    
    # This is the commit message #5:
    
    sort imports
    
    # This is the commit message #6:
    
    compile yara rules when extractor is initialised
    
    # This is the commit message #7:
    
    cache extractors in memory
    
    # This is the commit message #8:
    
    partially implement venv test
    
    # This is the commit message #9:
    
    fix issues with old maco installing automatically
    
    # This is the commit message #10:
    
    merge requirements install code
    
    # This is the commit message #11:
    
    fix benchmark

---
+++ tests/benchmark.py
@@ -0,0 +1,65 @@
+import cProfile
+import io
+import os
+import timeit
+
+from demo_extractors.complex import complex, complex_utils
+from maco import base_test
+
+instance = complex.Complex()
+
+class TestComplex(base_test.BaseTest):
+    """Test extractors work under default conditions."""
+    name = "Complex"
+    path = os.path.join(__file__, "../../demo_extractors")
+    create_venv=False
+
+    def test_auto_extract(self):
+        """Tests that we can run an extractor through maco."""
+        inputs = self.load_cart("data/trigger_complex.txt.cart")
+        inputs.seek(0)
+        ret = self.extract(inputs)
+        self.assertEqual(ret["family"], "complex")
+        self.assertEqual(ret["version"], "5")
+
+    def test_manual_extract(self):
+        """Tests that we can run an extractor through maco."""
+        inputs = self.load_cart("data/trigger_complex.txt.cart")
+        inputs.seek(0)
+        result = instance.run(inputs, [])
+        self.assertEqual(result.family, "complex")
+
+class TestComplexVenv(base_test.BaseTest):
+    """Test extractors work when run with virtual environments."""
+    name = "Complex"
+    path = os.path.join(__file__, "../../demo_extractors")
+    create_venv=True
+
+    def test_auto_extract(self):
+        """Tests that we can run an extractor through maco."""
+        inputs = self.load_cart("data/trigger_complex.txt.cart")
+        inputs.seek(0)
+        ret = self.extract(inputs)
+        self.assertEqual(ret["family"], "complex")
+        self.assertEqual(ret["version"], "5")
+
+def make():
+    TestComplex.setUpClass()
+    tc = TestComplex()
+    tc.setUp()
+    return tc
+
+def make_venv():
+    TestComplexVenv.setUpClass()
+    tc = TestComplexVenv()
+    tc.setUp()
+    return tc
+
+
+if __name__ == "__main__":
+    print("bypass hack - synthetic comparison (directly import and execute extractor)")
+    print(timeit.timeit("tc.test_manual_extract()", setup="from __main__ import make; tc=make()", number=10))
+    print("maco no venv isolation")
+    print(timeit.timeit("tc.test_auto_extract()", setup="from __main__ import make; tc=make()", number=10))
+    print("maco venv isolation")
+    print(timeit.timeit("tc.test_auto_extract()", setup="from __main__ import make_venv; tc=make_venv()", number=10))
